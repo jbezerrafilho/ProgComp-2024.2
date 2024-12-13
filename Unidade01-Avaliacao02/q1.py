26fd35611ff190ac2e02080fec5f3c62dcaa69d7
José Bezerra <50915483+jbezerrafilho@users.noreply.github.com>
2024-12-13 23:30:58 +0000

    Change q1.py

--- Unidade01-Avaliacao02/q1.py
+++ Unidade01-Avaliacao02/q1.py
@@ -1,1 +1,10 @@
-print('Olá mundo!')
+contador = 0
+
+num_inicial = 10
+num_final = 987631
+
+#for num in range(num_inicial, num_final + 1):
+
+dig_e = num_inicial % 10
+num_inicial = num_inicial // 10
+if 
