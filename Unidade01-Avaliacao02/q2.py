d4e7b2b03d11535783c7ddb2a62713a6bbbe8967
José Bezerra <jbezerra.filho@outlook.com>
2024-12-15 16:46:52 -0300

    change q2

---
+++ Unidade01-Avaliacao02/q2.py
@@ -0,0 +1,29 @@
+num_inicial = 10
+num_final = 100000
+contador = 0
+msg = f'No intervalo de {num_inicial} a {num_final}'
+
+# Este laço repete 'alguma coisa' dentro do intervalo que
+# vai do número inicial até o número final dado no problema
+while num_inicial <= num_final:
+  
+    direto = num_inicial
+    num_temp = num_inicial
+    inverso = 0
+
+    # Repita algo enquanto a variável > 0
+    while num_temp > 0: 
+        # Pegamos cada dígito do numero
+        digito = num_temp % 10
+        # E invertemos...
+        inverso = inverso * 10 + digito
+        # Diminuimos a gandeza da variável temp
+        num_temp = num_temp // 10
+
+    # Verificando se o número é palíndromo    
+    if direto == inverso:
+        contador += 1
+    # Importante incrementar a variável de controle para o while externo não ficar em loop 
+    num_inicial += 1
+
+print(f'{msg} temos {contador} números palíndromos')
