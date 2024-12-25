55bfd96bb6e79ef949fbf1ed53429979ffc3b781
José Bezerra <jbezerra.filho@outlook.com>
2024-12-25 15:55:20 -0300

    Change q1 and gitignore

--- Unidade01-Avaliacao02/q1.py
+++ Unidade01-Avaliacao02/q1.py
@@ -1,4 +1,3 @@
-
 num_inicial = 10
 num_final = 987631
 contador = 0
@@ -6,17 +5,16 @@ contador = 0
 # Este laço repete 'alguma coisa' dentro do intervalo que
 # vai do número inicial até o número final dado no problema
 while num_inicial <= num_final:
-    #num_temp é uma variável temporária para ser manipulada no laço interno
-    #onde manipularmeos os digitos do número
+
+    # num_temp é uma variável temporária para ser manipulada no laço interno
+    # onde manipularmeos os digitos do número
     num_temp = num_inicial
 
-    # Comecei errando o lugar desta variável colocando-a fora do while externo
-    # Quando o número não era decrescente, por estar fora do laço, não tinha como
-    # resetá-la para true. Percebi e a coloquei no lugar correto
+
     eh_decrescente = True
 
-    # Esta laço verifica se um número específico é descrescente 
-    while eh_decrescente and num_temp >= 10: 
+    # Este laço valida se um número é descrescente
+    while eh_decrescente and num_temp >= 10:
 
         # lsd -> Least Significant Digit (Dígito menos signicativo)
         lsd = num_temp % 10
@@ -27,13 +25,12 @@ while num_inicial <= num_final:
 
         if lsd > msd:
             eh_decrescente = False
-        # Saindo do laço interno
+            # Essa variável como 'False' interrompe o laço interno
 
     if eh_decrescente:
         contador += 1
-    # Importante incrementar a variável de controle para o while externo não ficar em loop 
+    # Importante incrementar a variável de controle para o while externo não ficar em loop
     num_inicial += 1
 
-# Galileu, perdoe o excesso de comentários. Fica mais fácil o entendimento
 print(f'No intervalo de 10 a 987631 \
-temos {contador} númeoros decrescentes')
+temos {contador} números decrescentes')
