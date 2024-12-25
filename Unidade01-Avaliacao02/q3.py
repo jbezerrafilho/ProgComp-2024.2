0f24923b935f9ae03babf9cfd9ae58bfbe9b4fa9
José Bezerra <jbezerra.filho@outlook.com>
2024-12-25 16:51:38 -0300

    Change q3.py

--- Unidade01-Avaliacao02/q3.py
+++ Unidade01-Avaliacao02/q3.py
@@ -1,25 +1,30 @@
-num = int((input('Digite um número para ver quantos primos existe entre 3 e ele: ')))
+limite = 100000
 contador = 0
 primo_anterior = 0
 primo_atual = 0
 
-i = 3
-while i <= num:   
-    # Esta laço interno testa se 'i'é primo
-    # 'j' representa os possíveis divisores de 'i' 
-    #Comecei com 2, pois todo número é divisível por 1
-    #Por isso ndiv já começa com 1 divisor
-    j = 2     
+dividendo = 3
+while dividendo <= limite:
+
+    # Divisor
+    div = 2
+    # Número de divisores
     ndiv = 1  
-    while j <= i:
-        if i % j == 0:
+    while div <= dividendo:
+        if dividendo % div == 0:
             ndiv += 1
-        j += 1
+        div += 1
 
-    #Validamos se o 'i' é primo e incrementamos 'qtd_primos'    
+    # Se for primo atualize as variáveis
     if ndiv == 2:
-        primo_atual = i
-    i += 2
+        primo_anterior = primo_atual
+        primo_atual = dividendo
+        if (primo_atual - primo_anterior) == 2:
+            contador += 1
+            print( f'({primo_anterior}, {primo_atual})')
 
-print(f'A soma de primos no intervalo até {num} \
-é {contador}.')
+    dividendo += 1
+
+
+print(f'A soma dos pares primos (ímpares consecutivos) menores que {limite} \
+é {contador} pares.')
