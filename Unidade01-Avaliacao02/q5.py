c92d60ff316398675096c12ec2393c4a2eb4e6af
José Bezerra <jbezerra.filho@outlook.com>
2025-01-05 19:11:12 -0300

    Add functionality to count Saturdays between two dates in q5.py

--- Unidade01-Avaliacao02/q5.py
+++ Unidade01-Avaliacao02/q5.py
@@ -37,8 +37,33 @@ for mes in range(1, mes_atual + 1):
     elif mes in [1, 5, 7, 8, 10, 12]: 
         qtd_dias += 31 
     elif mes in [4, 6, 9, 11]:
-        qtd_dias += 30           
-print(qtd_dias)
+        qtd_dias += 30   
+
+# Inicializando o dia da semana (27 de abril de 1968 é um sábado)
+qtd_sabados = 0
+dia_da_semana = 5  # 0 = segunda, 1 = terça, ..., 5 = sábado, 6 = domingo
+for ano in range(ano_0, ano_atual + 1):
+    for mes in range(1, 13):
+        if mes in [1, 3, 5, 7, 8, 10, 12]:
+            dias_no_mes = 31
+        elif mes in [4, 6, 9, 11]:
+            dias_no_mes = 30
+        elif mes == 2:
+            if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+                dias_no_mes = 29
+            else:
+                dias_no_mes = 28
+
+        for dia in range(1, dias_no_mes + 1):
+            if ano == ano_0 and mes == mes_0 and dia < dia_0:
+                continue
+            if ano == ano_atual and mes == mes_atual and dia > dia_atual:
+                break
+            if dia_da_semana == 5:  # 5 representa sábado
+                qtd_sabados += 1
+            dia_da_semana = (dia_da_semana + 1) % 7  # Atualiza o dia da semana
+
 
 
 print(f"Quantidade de dias entre {dia_0}/{mes_0}/{ano_0} e {dia_atual}/{mes_atual}/{ano_atual}: {qtd_dias} dias")
+print(f'A quantidade de sábados entre {dia_0}/{mes_0}/{ano_0} e {dia_atual}/{mes_atual}/{ano_atual} é: {qtd_sabados}')
