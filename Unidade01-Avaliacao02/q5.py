1363f09c2bab6a546eae1affa5204c280b88d054
José Bezerra <jbezerra.filho@outlook.com>
2025-01-05 21:28:09 -0300

    Refactor q5.py to simplify date calculations and improve Saturday counting logic

--- Unidade01-Avaliacao02/q5.py
+++ Unidade01-Avaliacao02/q5.py
@@ -9,11 +9,8 @@ dia_atual = date.day
 mes_atual = date.month
 ano_atual = date.year
 
-# Data Zero
-dia_0 = 27
-mes_0 = 4
-ano_0 = 1968
-
+# Data Inicial
+dia_0, mes_0, ano_0 = 27, 4, 1968 # Por ser dia 0, não será incluso na contagem
 qtd_dias = 0
 
 # Calculando os dias completos dos anos intermediários
@@ -25,16 +22,28 @@ for ano in range(ano_0 + 1, 2025):
         qtd_dias += 365
 
 
-
 # Calculando os dias do ano zero
 # >27/04/1968----------Dias calculados-----------31/12/1968<
-qtd_dias += 30 - dia_0
 for mes in range(mes_0, 13):
-    if mes in [5, 7, 8, 10, 12]:
+    if mes == mes_0 and mes in [1, 3, 5, 7, 8, 10, 12]:
+        qtd_dias += 31 - dia_0 
+    elif mes == mes_0 and mes in [4, 6, 9, 11]:
+        qtd_dias += 30 - dia_0 
+    elif mes == mes_0 and mes == 2:
+        if (ano_0 % 4 == 0 and ano_0 % 100 != 0) or (ano_0 % 400 == 0):
+            qtd_dias += 29 - dia_0 
+        else:
+            qtd_dias += 28 - dia_0 
+    elif mes in [1, 3, 5, 7, 8, 10, 12]:
         qtd_dias += 31 
-    elif mes in [6, 9, 11]:
+    elif mes in [4, 6, 9, 11]:
         qtd_dias += 30 
-#
+    elif mes == 2:
+        if (ano_0 % 4 == 0 and ano_0 % 100 != 0) or (ano_0 % 400 == 0):
+            qtd_dias += 29
+        else:
+            qtd_dias += 28
+
 
 # Calculando os dias do ano atual   
 # >01/01/2025----------Dias calculados-----------data_atual<
@@ -44,31 +53,21 @@ for mes in range(1, mes_atual + 1):
     elif mes in [1, 5, 7, 8, 10, 12]: 
         qtd_dias += 31 
     elif mes in [4, 6, 9, 11]:
-        qtd_dias += 30   
+        qtd_dias += 30 
+    elif mes == 2:
+        qtd_dias += 28 if (ano_atual % 4 != 0) or \
+            (ano_atual % 100 == 0 and ano_atual % 400 != 0) else 29
 
-# Calculando a quantidade de sábados
+# Ajuste final para corrigir os limites de intervalo
+
 qtd_sabados = 0
-dia_da_semana = 0  # 0 = segunda, 1 = terça, ..., 5 = sábado, 6 = domingo
-for ano in range(ano_0, ano_atual + 1):
-    for mes in range(1, 13):
-        if mes in [1, 3, 5, 7, 8, 10, 12]:
-            dias_no_mes = 31
-        elif mes in [4, 6, 9, 11]:
-            dias_no_mes = 30
-        elif mes == 2:
-            if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
-                dias_no_mes = 29
-            else:
-                dias_no_mes = 28
+dia_da_semana = 5  # 27/04/1968 é um sábado
 
-        for dia in range(1, dias_no_mes + 1):
-            if ano == ano_0 and mes == mes_0 and dia < dia_0:
-                continue
-            if ano == ano_atual and mes == mes_atual and dia > dia_atual:
-                break
-            if dia_da_semana == 5:  # 5 representa sábado
-                qtd_sabados += 1
-            dia_da_semana = (dia_da_semana + 1) % 7  # Atualiza o dia da semana
+# A divisão inteira de (qtd_dias + 6) por 7 é a quantidade de sábados
+# O '+6' é para arredondar para cima, pois a divisão inteira arredonda para baixo
+# Podemos fazer isso porque o dia 0 é um sábado
+qtd_sabados = (qtd_dias + 6) // 7
+
 
 
 
