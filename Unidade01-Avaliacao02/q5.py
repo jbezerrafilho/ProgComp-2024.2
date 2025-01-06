ed6da7e082ad9111a06c0b56d2da5915f6c0ee17
José Bezerra <jbezerra.filho@outlook.com>
2025-01-05 21:32:17 -0300

    Refactor q5.py to improve variable naming and enhance clarity in date calculations

--- Unidade01-Avaliacao02/q5.py
+++ Unidade01-Avaliacao02/q5.py
@@ -10,36 +10,35 @@ mes_atual = date.month
 ano_atual = date.year
 
 # Data Inicial
-dia_0, mes_0, ano_0 = 27, 4, 1968 # Por ser dia 0, não será incluso na contagem
+dia_inicial, mes_inicial, ano_inicial = 27, 4, 1968 # Por ser dia 0, não será incluso na contagem
 qtd_dias = 0
 
 # Calculando os dias completos dos anos intermediários
 # >1969---------------Dias calculados------------------2024<
-for ano in range(ano_0 + 1, 2025):
+for ano in range(ano_inicial + 1, 2025):
    if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
         qtd_dias += 366
    else:
         qtd_dias += 365
 
-
-# Calculando os dias do ano zero
+# Calculando os dias do ano inicial
 # >27/04/1968----------Dias calculados-----------31/12/1968<
-for mes in range(mes_0, 13):
-    if mes == mes_0 and mes in [1, 3, 5, 7, 8, 10, 12]:
-        qtd_dias += 31 - dia_0 
-    elif mes == mes_0 and mes in [4, 6, 9, 11]:
-        qtd_dias += 30 - dia_0 
-    elif mes == mes_0 and mes == 2:
-        if (ano_0 % 4 == 0 and ano_0 % 100 != 0) or (ano_0 % 400 == 0):
-            qtd_dias += 29 - dia_0 
+for mes in range(mes_inicial, 13):
+    if mes == mes_inicial and mes in [1, 3, 5, 7, 8, 10, 12]:
+        qtd_dias += 31 - dia_inicial 
+    elif mes == mes_inicial and mes in [4, 6, 9, 11]:
+        qtd_dias += 30 - dia_inicial 
+    elif mes == mes_inicial and mes == 2:
+        if (ano_inicial % 4 == 0 and ano_inicial % 100 != 0) or (ano_inicial % 400 == 0):
+            qtd_dias += 29 - dia_inicial 
         else:
-            qtd_dias += 28 - dia_0 
+            qtd_dias += 28 - dia_inicial 
     elif mes in [1, 3, 5, 7, 8, 10, 12]:
         qtd_dias += 31 
     elif mes in [4, 6, 9, 11]:
         qtd_dias += 30 
     elif mes == 2:
-        if (ano_0 % 4 == 0 and ano_0 % 100 != 0) or (ano_0 % 400 == 0):
+        if (ano_inicial % 4 == 0 and ano_inicial % 100 != 0) or (ano_inicial % 400 == 0):
             qtd_dias += 29
         else:
             qtd_dias += 28
@@ -58,10 +57,7 @@ for mes in range(1, mes_atual + 1):
         qtd_dias += 28 if (ano_atual % 4 != 0) or \
             (ano_atual % 100 == 0 and ano_atual % 400 != 0) else 29
 
-# Ajuste final para corrigir os limites de intervalo
-
 qtd_sabados = 0
-dia_da_semana = 5  # 27/04/1968 é um sábado
 
 # A divisão inteira de (qtd_dias + 6) por 7 é a quantidade de sábados
 # O '+6' é para arredondar para cima, pois a divisão inteira arredonda para baixo
@@ -69,7 +65,5 @@ dia_da_semana = 5  # 27/04/1968 é um sábado
 qtd_sabados = (qtd_dias + 6) // 7
 
 
-
-
-print(f"Quantidade de dias entre {dia_0}/{mes_0}/{ano_0} e {dia_atual}/{mes_atual}/{ano_atual}: {qtd_dias} dias")
+print(f"Quantidade de dias entre {dia_inicial}/{mes_inicial}/{ano_inicial} e {dia_atual}/{mes_atual}/{ano_atual}: {qtd_dias} dias")
 print(f'A quantidade de sábados no intervalo acima é: {qtd_sabados}')
