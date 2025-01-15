57da4d484fcf0c2286576c8119da11f28400218b
José Bezerra <jbezerra.filho@outlook.com>
2025-01-15 10:21:13 -0300

    Refactor max_path.py to improve variable naming and enhance clarity in path sum calculation

--- Exercicios/projecteuler.net/18. max_path.py
+++ Exercicios/projecteuler.net/18. max_path.py
@@ -5,17 +5,18 @@ caminhos = [
  [8, 5, 9, 3]
  ]
 
-temp = [3]
+path_to_max_sum = [3]
+
 
 for l in range(1, len(caminhos)):
     caminhos[l][0] += caminhos[l-1][0] # Primeiro elemento
     for c in range(1, len(caminhos[l])-1): # Elementos do meio
         caminhos[l][c] += max(caminhos[l-1][c-1], caminhos[l-1][c])
     caminhos[l][l] += caminhos[l-1][l-1] # Ultimo elemento
-    temp.append(max(caminhos[l]))   
-    
+    path_to_max_sum.append(max(caminhos[l]) - max(caminhos[l-1]))   
+
 # caminhos[-1] é a ultima linha da matriz ou último elemento de uma lista
 # Equivalente a caminhos[len(caminhos)-1]
 print(max(caminhos[-1])) 
-print(temp)
+print(path_to_max_sum)
 
