25642c66c8930a10277c0f36e694397d8e61c2c3
José Bezerra <jbezerra.filho@outlook.com>
2025-01-15 08:08:05 -0300

    Refactor troco calculation to use a loop for denominations

--- Exercicios/01_.troco-v1.py
+++ Exercicios/01_.troco-v1.py
@@ -3,38 +3,43 @@ pago = int(input('Quanto o cliente pagou? '))
 troco = pago - conta
 troco_variavel = troco
 
-#Calculando quantas cédulas e moeda de cada denominação
-cedula200 = troco_variavel // 200
-troco_variavel = troco_variavel % 200
+# Denominações de moedas
+c200, c100, c50, c20, c10, c5, c2, m1 = 0, 0, 0, 0, 0, 0, 0, 0
 
-cedula100 = troco_variavel // 100
-troco_variavel = troco_variavel % 100
+for t in [200, 100, 50, 20, 10, 5, 2, 1]:
+    if troco_variavel >= 200:
+        c200 = troco_variavel // 200
+        troco_variavel = troco_variavel % 200
+    elif troco_variavel >= 100:
+        c100 = troco_variavel // 100
+        troco_variavel = troco_variavel % 100
+    elif troco_variavel >= 50:
+        c50 = troco_variavel // 50
+        troco_variavel = troco_variavel % 50
+    elif troco_variavel >= 20:
+        c20 = troco_variavel // 20
+        troco_variavel = troco_variavel % 20
+    elif troco_variavel >= 10:
+        c10 = troco_variavel // 10
+        troco_variavel = troco_variavel % 10
+    elif troco_variavel >= 5:
+        c5 = troco_variavel // 5
+        troco_variavel = troco_variavel % 5
+    elif troco_variavel >= 2:
+        c2 = troco_variavel // 2
+        troco_variavel = troco_variavel % 2
+    else:
+        m1 = troco_variavel
 
-cedula50 = troco_variavel // 50
-troco_variavel = troco_variavel % 50
-
-cedula20 = troco_variavel // 20
-troco_variavel = troco_variavel % 20
-
-cedula10 = troco_variavel // 10
-troco_variavel = troco_variavel % 10
-
-cedula5 = troco_variavel // 5
-troco_variavel = troco_variavel % 5
-
-cedula2 = troco_variavel // 2
-troco_variavel = troco_variavel % 2
-
-moeda1 = troco_variavel
 
 print()
 print("Troco: R$", troco)
 print()
-print("Cédula R$ 200:", cedula200)
-print("Cédula R$ 100:", cedula100)
-print("Cédula R$ 50: ", cedula50)
-print("Cédula R$ 20: ", cedula20)
-print("Cédula R$ 10: ", cedula10)
-print("Cédula R$ 5:  ", cedula5)
-print("Cédula R$ 2:  ", cedula2)
-print("Moeda  R$ 1:  ", moeda1)
+print("Cédula R$ 200:", c200)
+print("Cédula R$ 100:", c100)
+print("Cédula R$ 50: ", c50)
+print("Cédula R$ 20: ", c20)
+print("Cédula R$ 10: ", c10)
+print("Cédula R$ 5:  ", c5)
+print("Cédula R$ 2:  ", c2)
+print("Moeda  R$ 1:  ", m1)
