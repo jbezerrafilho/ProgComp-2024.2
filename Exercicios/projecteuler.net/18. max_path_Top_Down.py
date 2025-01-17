3253e97b5aaff0c1df801cc244c1afa9ec37f28a
José Bezerra <jbezerra.filho@outlook.com>
2025-01-17 10:00:25 -0300

    Rename max_path.py to max_path_Top_Down.py and refactor path sum calculation for improved clarity

--- Exercicios/projecteuler.net/18. max_path_Top_Down.py
+++ Exercicios/projecteuler.net/18. max_path_Top_Down.py
@@ -20,12 +20,13 @@ caminhos = [
 path_to_max_sum = [75]
 
 
-for l in range(1, len(caminhos)):
-    caminhos[l][0] += caminhos[l-1][0] # Primeiro elemento
-    for c in range(1, len(caminhos[l])-1): # Elementos do meio
-        caminhos[l][c] += max(caminhos[l-1][c-1], caminhos[l-1][c])
-    caminhos[l][l] += caminhos[l-1][l-1] # Ultimo elemento
-    path_to_max_sum.append(max(caminhos[l]) -  max(caminhos[l-1]) )   
+for j in range(1, len(caminhos)):
+    caminhos[j][0] += caminhos[j-1][0] # Primeiro elemento
+    # Percorre os elementos do meio
+    for c in range(1, len(caminhos[j])-1): # Elementos do meio
+        caminhos[j][c] += max(caminhos[j-1][c-1], caminhos[j-1][c])
+    caminhos[j][j] += caminhos[j-1][j-1] # Ultimo elemento
+    path_to_max_sum.append(max(caminhos[j]) -  max(caminhos[j-1]))   
 
 # caminhos[-1] é a ultima linha da matriz ou último elemento de uma lista
 # Equivalente a caminhos[len(caminhos)-1]
