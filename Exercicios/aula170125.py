00b23991c8035428d721ebdb1826f61da8ea7b6e
José Bezerra <50915483+jbezerrafilho@users.noreply.github.com>
2025-01-17 23:27:35 +0000

    Adicionar links de referência e explicações sobre funções map, filter e reduce em aula170125.py

--- Exercicios/aula170125.py
+++ Exercicios/aula170125.py
@@ -1,2 +1,6 @@
 https://larissacroco.medium.com/funções-map-reduce-filter-e-expressão-lambda-16b47f36277d
 https://dontpad.com/pcomp2024
+
+map(função, sequência) - aplica a função a todos os itens da sequência
+filter(função, sequência) - retorna uma sequência com os itens que retornam True
+reduce(função, sequência) - aplica a função a todos os itens da sequência, de forma cumulativaprint("Maior soma:", triangle[0][0])
