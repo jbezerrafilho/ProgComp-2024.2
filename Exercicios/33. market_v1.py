b45959f0f7994103b3775ba996c365b044401330
José Bezerra <jbezerra.filho@outlook.com>
2025-02-02 17:00:41 -0300

    Add script to plot historical stock prices using yfinance and matplotlib

---
+++ Exercicios/33. market_v1.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+ticker = 'PETR4.SA'
+df = yf.download( ticker,  period='1y')
+
+#Criar o gráfico
+plt.plot(df.index, df["Close"], "purple")
+
+# Configurações do gráfico
+plt.title(f"Histórico de Preços - {ticker}")
+plt.xlabel("Data")
+plt.ylabel("Preço (R$)")
+plt.xticks(rotation=45)
+
+# Mostrar o gráfico
+plt.show()
